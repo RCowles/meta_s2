e5e50a0036d818dee96a901d001bfd0120abcfda
RCowles <contact@ryanscowles.com>
2013-10-13 13:05:09 -0700

    Simplified header layout
    Centered branding, moved menu to its own line… some other stuff… I have
    no idea what I'm doing.

--- header.php
+++ header.php
@@ -45,7 +45,7 @@
 		<?php $header_image = get_header_image();
 		if ( ! empty( $header_image ) ) { ?>
 			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-				<img src="<?php header_image(); ?>" width="<?php echo /*get_custom_header()->width;*/ "100%" ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
+				<img src="<?php header_image(); ?>" width="<?php echo /*get_custom_header()->width;*/ "100%" ?>" height="<?php echo get_custom_header()->height; ?>" alt="" class="banner">
 			</a>
 		<?php } // if ( ! empty( $header_image ) ) ?>
 	<nav class="sub-navigation">
