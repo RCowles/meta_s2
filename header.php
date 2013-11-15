3e665c2836387a025458499cdfe755dd30c79432
RCowles <contact@ryanscowles.com>
2013-11-14 19:59:01 -0800

    Fixed typo

--- header.php
+++ header.php
@@ -33,7 +33,7 @@
 				
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<h1 class="menu-toggle"><?php _e( 'Menu', 'meta_s2' ); ?></h1>
-			<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'S	kip to content', 'meta_s2' ); ?>"><?php _e( 'Skip to content', 'meta_s2' ); ?></a></div>
+			<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'meta_s2' ); ?>"><?php _e( 'Skip to content', 'meta_s2' ); ?></a></div>
 
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
