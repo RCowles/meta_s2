e4c4d1476aa495e3ebe26c942fa8c5289c4c2af1
RCowles <contact@ryanscowles.com>
2013-11-29 13:14:38 -0800

    Add Genericons and clean some stuff up

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -183,7 +183,7 @@ function meta_s2_posted_on() {
 		esc_html( get_the_modified_date() )
 	);
 
-	printf( __( '<span class="posted-on">Posted on %1$s</span><span class="byline"> by %2$s</span>', 'meta_s2' ),
+	printf( __( '<span class="posted-on">%1$s</span><span class="byline">%2$s</span>', 'meta_s2' ),
 		sprintf( '<a href="%1$s" title="%2$s" rel="bookmark">%3$s</a>',
 			esc_url( get_permalink() ),
 			esc_attr( get_the_time() ),
